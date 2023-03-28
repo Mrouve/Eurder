ff8eb003d0e6dc5873d59c8a7b8f82d819a15665
Mrouve <maximerouve@hotmail.com>
2023-03-28 14:17:09 +0200

    [IMP] - CUSTOMER - Add mapping for list of toDto objects

--- Eurder/src/main/java/com/switchfully/eurder/customer/service/mappers/CustomerMapper.java
+++ Eurder/src/main/java/com/switchfully/eurder/customer/service/mappers/CustomerMapper.java
@@ -2,7 +2,12 @@ package com.switchfully.eurder.customer.service.mappers;
 
 import com.switchfully.eurder.customer.domain.Customer;
 import com.switchfully.eurder.customer.service.dtos.CreateCustomerDto;
+import com.switchfully.eurder.customer.service.dtos.CustomerDto;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
 public class CustomerMapper {
     public Customer fromDto(CreateCustomerDto customerDto) {
         Customer customer = new Customer.CustomerBuilder()
@@ -13,6 +18,18 @@ public class CustomerMapper {
                 .withPhoneNumber(customerDto.getPhoneNumber())
                 .build();
         return customer;
+    }
+
+    public CustomerDto toDto(Customer customer){
+        return new CustomerDto(customer.getUuid(), customer.getFirstname(), customer.getLastname(),
+                customer.getEmail(), customer.getAddress(), customer.getPhoneNumber());
+    }
+
+    public List<CustomerDto> toDtoList(List<Customer> listOfCustomers){
+        return listOfCustomers
+                .stream()
+                .map(this::toDto)
+                .toList();
     }
 }
 
