b430d4aaf4abdc24f07106d704526d3903aa0588
Mrouve <maximerouve@hotmail.com>
2023-03-29 13:47:58 +0200

    [ADD] - ITEM - Add methods for feature "add Item" in repo, service, controller ; added missing @keywords

--- Eurder/src/main/java/com/switchfully/eurder/item/service/mappers/ItemMapper.java
+++ Eurder/src/main/java/com/switchfully/eurder/item/service/mappers/ItemMapper.java
@@ -3,7 +3,9 @@ package com.switchfully.eurder.item.service.mappers;
 import com.switchfully.eurder.item.domain.Item;
 import com.switchfully.eurder.item.service.dtos.CreateItemDto;
 import com.switchfully.eurder.item.service.dtos.ItemDto;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ItemMapper {
     public Item fromDto(CreateItemDto createItemDto){
         return new Item(createItemDto.getItemName(), createItemDto.getItemDescription(), createItemDto.getItemPrice(),createItemDto.getItemInStock());
